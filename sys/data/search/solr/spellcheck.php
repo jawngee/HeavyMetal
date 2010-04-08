db890d05cf4d6a9a5174024358f0ab5d9fc84d1d
Peter <dev@pdm.(none)>
2010-04-08 17:55:17 -0400

    Fixed double-search bug when no results are returned

---
+++ sys/data/search/solr/spellcheck.php
@@ -0,0 +1,47 @@
+<?
+
+/*
+ *  Container for SOLR spellchecker information 
+ */
+
+
+class Spellcheck
+{
+	private $term;
+	private $termFrequency;
+	private $startOffset;
+	private $endOffset;
+	private $correctlySpelled;
+	private $collation;
+	
+	private $suggestions = array(); // List of suggested terms followed by frequency
+	
+	public function __construct($solrSpellcheck)
+	{
+		$this->correctlySpelled = $solrSpellcheck['suggestions']['correctlySpelled'];
+		$this->collation = $solrSpellcheck['suggestions']['collation'];
+
+		foreach($solrSpellcheck['suggestions'] as $term => $info)
+		{
+			$this->term = $term;
+			$this->termFrequency = $info['origFreq'];
+			$this->startOffset = $info['startOffset'];
+			$this->endOffset = $info['endOffset'];
+
+			foreach($info['suggestion'] as $sugg)
+			{
+				$this->suggestions[$sugg['word']] = $sugg['freq'];
+			}
+			
+			
+			break;  // only should be getting back one of these
+		}
+	}
+	
+	public function suggestion()
+	{
+		return ($this->correctlySpelled)?null:$this->collation;
+	}
+
+}
+ 
