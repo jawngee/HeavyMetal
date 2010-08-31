9a70f6902a558dce73d8f085f281e6627b5c5461
Jon Gilkison <jon.gilkison@gmail.com>
2010-08-31 02:44:38 -0400

    Rewrote the regex for parsing {{ }} in views to deal with closures:
    {{option('something',function($this){return 'hello';},'other')}}

--- sys/core/view.php
+++ sys/core/view.php
@@ -47,7 +47,8 @@ function get_view($view)
 {
 	$contents=file_get_contents($view.EXT);
 	$contents=preg_replace('#<\s*php\s*\:loop\s*for\s*=\s*"\s*([^\'\"]*)\s*"\s*as\s*=\s*"\s*(.*)\s*"\s*>\s*(.*)\s*<\s*\/\s*php\s*:\s*loop\s*>#','<? foreach($1 as $2):?>$3<?endforeach;?>',$contents);
-    $contents=preg_replace("|{{([^}]*)}}|m",'<?=$1?>',$contents);
+    $contents=preg_replace("|\{{2}([^}]*)\}{2}|is",'<?=$1?>',$contents);
+    $contents=preg_replace("|\{{2}(.*)\}{2}|is",'<?=$1?>',$contents); // for closures.
     return $contents;
 }
 
